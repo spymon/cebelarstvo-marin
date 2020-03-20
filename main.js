a23e81b3a5f0ef6041ffc122825466bc95e1e5e5
Simon Žižek <spymon12@gmail.com>
2020-03-20 19:54:21 +0100

    Added jQuery library, fixing shrinking header issue with jquery, the header responsive

--- main.js
+++ main.js
@@ -1,33 +1,13 @@
-window.onscroll = function() {
-  scrollFunction();
-  checkNewHeight();
-};
-
-/* Smooth scrolling */
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.querySelector('.logo').style.width = '180px';
-  } else {
-    document.querySelector('.logo').style.width = '300px';
-  }
-}
-
-/* changing height of the section based on height of the header */
-let headerHeight = 0;
-let showcase = document.querySelector('.showcase');
-let behindNav = document.querySelector('.behind-nav');
-let paddTop = document.querySelectorAll('.pd-top');
-
-function checkNewHeight() {
-  let headerHeight = document.querySelector('.header').offsetHeight;
-  showcase.style.minHeight = `calc(100vh - ${headerHeight}px)`;
-  behindNav.style.marginBottom = `${headerHeight}px`;
-
-  /* changing padding of sections by the height value of navbar height */
-  paddTop.forEach(section => {
-    let total = headerHeight + 20;
-    section.style.padding = `${total}px 0 2rem`;
+$(document).ready(function() {
+  $(document).on('scroll', function() {
+    if ($(document).scrollTop() > 100) {
+      $('header').addClass('shrink');
+    } else {
+      $('header').removeClass('shrink');
+    }
   });
-}
 
-checkNewHeight();
+  $('.logo').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 200);
+  });
+});

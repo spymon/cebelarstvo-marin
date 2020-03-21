ce3f33e3de7a51f7b404e8c6c5d79dd42206c982
Simon Žižek <spymon12@gmail.com>
2020-03-21 20:04:45 +0100

    Add arrow on show case and change showcage img

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
   $(document).on('scroll', function() {
     if ($(document).scrollTop() > 100) {
       $('header').addClass('shrink');
+      $('.down-arrow').fadeOut();
     } else {
       $('header').removeClass('shrink');
+      $('.down-arrow').fadeIn();
     }
   });
 
